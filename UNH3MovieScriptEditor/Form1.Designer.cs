c107d7824adea8356a6bf079ff1184ee5085aa50
zMath3usMSF <48952393+zMath3usMSF@users.noreply.github.com>
2024-10-27 20:43:10 -0300

    Adicionar arquivos de projeto.

---
+++ UNH3MovieScriptEditor/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace UNH3MovieScriptEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.movieScriptToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.utilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportAllToTXTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importAllFromTxtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCurrentText = new System.Windows.Forms.Label();
+            this.lblNumberOfTexts = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.movieScriptToolStripMenuItem,
+            this.utilToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(702, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // movieScriptToolStripMenuItem
+            // 
+            this.movieScriptToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.movieScriptToolStripMenuItem.Name = "movieScriptToolStripMenuItem";
+            this.movieScriptToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.movieScriptToolStripMenuItem.Text = "Movie Script";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // utilToolStripMenuItem
+            // 
+            this.utilToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exportAllToTXTToolStripMenuItem,
+            this.importAllFromTxtToolStripMenuItem});
+            this.utilToolStripMenuItem.Name = "utilToolStripMenuItem";
+            this.utilToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.utilToolStripMenuItem.Text = "Util";
+            // 
+            // exportAllToTXTToolStripMenuItem
+            // 
+            this.exportAllToTXTToolStripMenuItem.Name = "exportAllToTXTToolStripMenuItem";
+            this.exportAllToTXTToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.exportAllToTXTToolStripMenuItem.Text = "Export All to TXT";
+            this.exportAllToTXTToolStripMenuItem.Click += new System.EventHandler(this.exportAllToTXTToolStripMenuItem_Click);
+            // 
+            // importAllFromTxtToolStripMenuItem
+            // 
+            this.importAllFromTxtToolStripMenuItem.Name = "importAllFromTxtToolStripMenuItem";
+            this.importAllFromTxtToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.importAllFromTxtToolStripMenuItem.Text = "Import All from Txt";
+            this.importAllFromTxtToolStripMenuItem.Click += new System.EventHandler(this.importAllFromTxtToolStripMenuItem_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(240, 277);
+            this.listBox1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(268, 65);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(419, 211);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(612, 282);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(463, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Current Text:";
+            // 
+            // lblCurrentText
+            // 
+            this.lblCurrentText.AutoSize = true;
+            this.lblCurrentText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblCurrentText.Location = new System.Drawing.Point(537, 28);
+            this.lblCurrentText.Name = "lblCurrentText";
+            this.lblCurrentText.Size = new System.Drawing.Size(13, 13);
+            this.lblCurrentText.TabIndex = 5;
+            this.lblCurrentText.Text = "?";
+            // 
+            // lblNumberOfTexts
+            // 
+            this.lblNumberOfTexts.AutoSize = true;
+            this.lblNumberOfTexts.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblNumberOfTexts.Location = new System.Drawing.Point(660, 28);
+            this.lblNumberOfTexts.Name = "lblNumberOfTexts";
+            this.lblNumberOfTexts.Size = new System.Drawing.Size(13, 13);
+            this.lblNumberOfTexts.TabIndex = 7;
+            this.lblNumberOfTexts.Text = "?";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(566, 28);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Number of Texts:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::UNH3MovieScriptEditor.Properties.Resources.Background;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(702, 285);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 314);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblNumberOfTexts);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblCurrentText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "UNH3 Movie Script Text Editor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem movieScriptToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnUpdate;
+        public System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCurrentText;
+        private System.Windows.Forms.Label lblNumberOfTexts;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ToolStripMenuItem utilToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportAllToTXTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importAllFromTxtToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ UNH3MovieScriptEditor/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UNH3MovieScriptEditor
+{
+    public partial class Form1 : Form
+    {
+        List<byte[]> scriptBlocks = new List<byte[]>();
+        List<string> texts = new List<string>();
+        string movieFileName;
+
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.SelectedIndexChanged +=ListBox1_SelectedIndexChanged;
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = listBox1.SelectedIndex;
+            richTextBox1.Text = texts[selectedIndex];
+            lblCurrentText.Text = $"{selectedIndex}";
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "UNH3 Movie Script (*.bin)|*.bin|All Files (*.*)|*.*";
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                movieFileName = Path.GetFileNameWithoutExtension(ofd.FileName);
+                ReadMovieScript(ofd.FileName);
+            }
+        }
+
+        private void ReadMovieScript(string fileName)
+        {
+            scriptBlocks = new List<byte[]>();
+            texts = new List<string>();
+            listBox1.Items.Clear();
+            pictureBox1.Visible = false;
+            MemoryStream ms = new MemoryStream(File.ReadAllBytes(fileName));
+
+            byte[] buffer = new byte[4];
+            ms.Read(buffer, 0, buffer.Length);
+            int textCount = BitConverter.ToInt32(buffer, 0);
+
+            for(int i = 0; i < textCount; i++)
+            {
+                byte[] scriptBlock = new byte[0x54];
+                ms.Read(scriptBlock, 0, scriptBlock.Length);
+                scriptBlocks.Add(scriptBlock);
+            }
+
+            for(int i = 0; i < textCount; i++)
+            {
+                List<byte> chars = new List<byte>();
+                while(true)
+                {
+                    int cByte = ms.ReadByte();
+                    if(cByte == 0)
+                    {
+                        texts.Add(Encoding.GetEncoding("ISO-8859-1").GetString(chars.ToArray()));
+                        break;
+                    }
+                    chars.Add((byte)cByte);
+                }
+            }
+
+            foreach(string text in texts)
+            {
+                listBox1.Items.Add(text);
+            }
+
+            lblNumberOfTexts.Text = $"{texts.Count}";
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndexChanged -=ListBox1_SelectedIndexChanged;
+            int selectedIndex = int.Parse(lblCurrentText.Text);
+            listBox1.Items[selectedIndex] = richTextBox1.Text;
+            texts[selectedIndex] = richTextBox1.Text;
+            listBox1.SelectedIndexChanged +=ListBox1_SelectedIndexChanged;
+        }
+
+        private void exportAllToTXTToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.FileName = movieFileName;
+            sfd.Filter = "Txt File (*.txt)|*.txt";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllLines(sfd.FileName, texts.ToArray());
+            }
+        }
+
+        private void importAllFromTxtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Txt File (*.txt)|*.txt|All Files (*.*)|*.*";
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                texts = File.ReadAllLines(ofd.FileName).ToList();
+                listBox1.Items.Clear();
+                foreach (string text in texts)
+                {
+                    listBox1.Items.Add(text);
+                }
+                MessageBox.Show("All texts have been imported successfully!");
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MemoryStream ms = new MemoryStream();
+            byte[] textCount = BitConverter.GetBytes(Convert.ToInt32(texts.Count));
+            ms.Write(textCount, 0, textCount.Length);
+            foreach (byte[] scriptBlock in scriptBlocks)
+            {
+                ms.Write(scriptBlock, 0, scriptBlock.Length);
+            }
+            foreach (string text in texts)
+            {
+                byte[] textBytes = Encoding.GetEncoding("ISO-8859-1").GetBytes(text);
+                ms.Write(textBytes, 0, textBytes.Length);
+                ms.WriteByte(0);
+            }
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.FileName = movieFileName;
+            sfd.Filter = "UNH3 Movie Script File (*.bin)|*.bin";
+            if(sfd.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllBytes(sfd.FileName, ms.ToArray());
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("UNH3 Movie Script Text Editor, version 1.0. \n\nMade by zMath3usMSF.");
+        }
+    }
+}
